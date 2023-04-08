49e3673e023d1eedc4f70d2433991809522f1042
Alex Suspiro <alexandresuspiro@gmail.com>
2023-04-08 19:28:02 +0100

    Setup bottle web app

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('hello world')
+from bottle import route, run, template
+
+@route('/')
+def home():
+    return '<h1>Hello World!</h1>'
+
+@route('/hello/<name>')
+def index(name):
+    return template('<h2>Hello {{name}}!</h2>', name=name)
+
+run(host='localhost', port=8081)
